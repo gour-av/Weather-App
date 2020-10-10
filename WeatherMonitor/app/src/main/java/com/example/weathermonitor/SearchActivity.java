b07acb580cbe1806a0eef430b3be9bdbafee6a3d
gouravguru <gourav97031@gmail.com>
2020-10-10 21:45:50 +0530

    Api Call Done for List of Location

--- WeatherMonitor/app/src/main/java/com/example/weathermonitor/SearchActivity.java
+++ WeatherMonitor/app/src/main/java/com/example/weathermonitor/SearchActivity.java
@@ -1,20 +1,40 @@
 package com.example.weathermonitor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.weathermonitor.api.ApiClient;
+import com.example.weathermonitor.api.Network;
+import com.example.weathermonitor.api.ResponseRequestType;
+
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /*
 Activity to search the weather report based on the input
  */
 public class SearchActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private ResponseAdapter responseAdapter;
+
+    private String Pass;
 
     private EditText et_search_bar;
     private Button btn_search_get_btn;
+    private ArrayList<ResponseRequestType> respons = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +42,15 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
         initViews();
         setOnclickListeners();
+        setRecyclerAdapter();
+    }
+
+    private void setRecyclerAdapter() {
+        responseAdapter = new ResponseAdapter(respons);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(manager);
+        mRecyclerView.setAdapter(responseAdapter);
+
     }
 
     private void setOnclickListeners() {
@@ -30,19 +59,37 @@ public class SearchActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                /*
-                Intents for going to the next page
-                 */
-                Intent searchIntent = new Intent(SearchActivity.this, DisplayWeatherActivity.class);
-                startActivity(searchIntent);
+           Pass =  et_search_bar.getText().toString();
+                ApiClient apiClient = Network.getInstance().create(ApiClient.class);
+                Call<ArrayList<ResponseRequestType>> call = apiClient.enterName(Pass);
+                call.enqueue(new Callback<ArrayList<ResponseRequestType>>() {
+                    @Override
+                    public void onResponse(Call<ArrayList<ResponseRequestType>> call, Response<ArrayList<ResponseRequestType>> response) {
+                        if (response.code()==HttpURLConnection.HTTP_OK){
+                            respons = response.body();
+                            responseAdapter.updateDataSet(respons);
+                            responseAdapter.notifyDataSetChanged();
+
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ArrayList<ResponseRequestType>> call, Throwable t) {
+                        Toast.makeText(SearchActivity.this,"Response Failed"+t.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
             }
         });
     }
 
-    private void initViews() {
+                    private void initViews() {
 
-        btn_search_get_btn = findViewById(R.id.btn_search_get_btn);
-        et_search_bar = findViewById(R.id.et_search_bar);
+                        btn_search_get_btn = findViewById(R.id.btn_search_get_btn);
+                        et_search_bar = findViewById(R.id.et_search_bar);
+                        mRecyclerView = findViewById(R.id.recycler);
 
-    }
-}
+                    }
+                }
+
