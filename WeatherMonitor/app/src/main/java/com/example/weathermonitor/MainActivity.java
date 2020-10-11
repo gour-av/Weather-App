53cc63f6d3bf6e590043845541d5e22bbdcc5117
Mukesh <mukeshkumar.1699@gmail.com>
2020-10-11 12:29:00 +0530

    UI parts Done

--- WeatherMonitor/app/src/main/java/com/example/weathermonitor/MainActivity.java
+++ WeatherMonitor/app/src/main/java/com/example/weathermonitor/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.weathermonitor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Handler mhandler = new Handler();
+    private Boolean IsFirst = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            mhandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    dostuff();
+                }
+            }, 5000);
+    }
+
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        mhandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //dostuff();
+            }
+        }, 1000);
+
+    }
+
+    private void dostuff() {
+
+        Bundle bundle = new Bundle();
+        bundle.putString("city", "Banglore");
+        bundle.putString("weoid", "220101");
+        Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
     }
 }
--- WeatherMonitor/app/src/main/java/com/example/weathermonitor/SearchActivity.java
+++ WeatherMonitor/app/src/main/java/com/example/weathermonitor/SearchActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -13,8 +15,10 @@ Activity to search the weather report based on the input
  */
 public class SearchActivity extends AppCompatActivity {
 
-    private EditText et_search_bar;
+    private AutoCompleteTextView atv_search_bar;
     private Button btn_search_get_btn;
+    private String []  hello  = new String [] { "sunday", "monday", "tuesday", "thursday" };
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,9 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
         initViews();
         setOnclickListeners();
+        adapter = new ArrayAdapter<>(SearchActivity.this, android.R.layout.simple_list_item_1,hello);
+        atv_search_bar.setAdapter(adapter);
+        atv_search_bar.setThreshold(1);
     }
 
     private void setOnclickListeners() {
@@ -42,7 +49,13 @@ public class SearchActivity extends AppCompatActivity {
     private void initViews() {
 
         btn_search_get_btn = findViewById(R.id.btn_search_get_btn);
-        et_search_bar = findViewById(R.id.et_search_bar);
+        atv_search_bar = (AutoCompleteTextView) findViewById(R.id.atv_search_bar);
 
+
+        Bundle bundle = getIntent().getExtras();
+        String venName = bundle.getString("city");
+
+        //atv_search_bar.setText(venName);
     }
+
 }
